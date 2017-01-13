a9b713ea35558c16ea7065ee05d761836501827c
Klas Mellbourn <klas.mellbourn@klarna.com>
2017-01-13 21:01:29 +0100

    added software update

--- sudo-install.sh
+++ sudo-install.sh
@@ -1,3 +1,9 @@
+# stop startup sound
 sudo nvram SystemAudioVolume=%80
 # Add Watch Simulator to Launchpad
 sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app" "/Applications/Simulator.app"
+
+###############################################################################
+# Update App Store apps
+###############################################################################
+sudo softwareupdate -i -a
